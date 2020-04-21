9156a65b5969e8a72744d211bbe2084484b0d7b5
Sturm Mabie <sturm@cryptm.org>
2020-04-21 01:14:28 -0400

    add map (') fn and modify parser to accept it

--- src/lib.ml
+++ src/lib.ml
@@ -15,8 +15,10 @@ and map2 ctxs xs ys f =
         else (
           Rt.push @ Array.get xs idx;
           Rt.push @ Array.get ys idx;
-          f ctxs; go (idx - 1)
-        ) in go @ Array.length xs  - 1
+          f ctxs;
+          go (idx - 1)
+        ) in
+      go @ Array.length xs  - 1
 
 and broadcast ctxs x ys f =
   with_list ctxs @
@@ -28,7 +30,7 @@ and broadcast ctxs x ys f =
         f ctxs; go (idx - 1)
       ) in go @ Array.length ys - 1
 
-and add ctxs =
+and add ctxs =                  (* + *)
   let x = Rt.pop_eval ctxs in
   let y = Rt.pop_eval ctxs in
   match x, y with
@@ -40,7 +42,7 @@ and add ctxs =
   | x, L ys | L ys, x -> broadcast ctxs x ys add
   | _ -> type_err "+"
 
-and sub ctxs = 
+and sub ctxs =                  (* - *)
   let x = Rt.pop_eval ctxs in
   let y = Rt.pop_eval ctxs in
   match x, y with
@@ -52,7 +54,7 @@ and sub ctxs =
   | x, L ys | L ys, x -> broadcast ctxs x ys sub
   | _ -> type_err "-"
 
-and mul ctxs =
+and mul ctxs =                  (* * *)
   let x = Rt.pop_eval ctxs in
   let y = Rt.pop_eval ctxs in
   
@@ -65,7 +67,7 @@ and mul ctxs =
   | x, L ys | L ys, x -> broadcast ctxs x ys mul
   | _ -> type_err "*"
 
-and div ctxs =
+and div ctxs =                  (* % *)
   let x = Rt.pop_eval ctxs in
   let y = Rt.pop_eval ctxs in
   match x, y with
@@ -77,14 +79,14 @@ and div ctxs =
   | x, L ys | L ys, x -> broadcast ctxs x ys div
   | _ -> type_err "%"
 
-and neg ctxs =
+and neg ctxs =                  (* neg *)
   Rt.push @
     match Rt.pop_eval ctxs with
     | Z x -> Z (-1 * x)
     | R x -> R (-1.0 *. x)
     | _ -> type_err "neg"
 
-and set ctxs =
+and set ctxs =                  (* : *)
   let q = Rt.pop () in
   let v = Rt.pop_get ctxs in
   match q, v with
@@ -94,13 +96,13 @@ and set ctxs =
       | None -> raise @ Failure "no context found")
   | _ -> type_err ":"
 
-and apply ctxs =
+and apply ctxs =                (* . *)
   match Rt.pop_get ctxs with
   | F { is_oper = _; instrs = _ } as f -> Rt.call_fn f ctxs
   | _ -> type_err "."
 
-and list_end ctxs = Rt.push N
-and list_begin ctxs =
+and list_end ctxs = Rt.push N   (* ] *)
+and list_begin ctxs =           (* [ *)
   let xs = Array.empty () in
   let rec go () =
     match Rt.pop () with
@@ -108,7 +110,7 @@ and list_begin ctxs =
     | x -> Array.add_one xs x; go () in
   go ()
 
-and fold ctxs =
+and fold ctxs =                 (* / *)
   let f = Rt.pop_get ctxs in
   let x = Rt.pop_eval ctxs in
   match f, x with
@@ -120,6 +122,23 @@ and fold ctxs =
      Rt.push @ Array.fold_left fn N xs
   | _ -> type_err "/"
 
+and map ctxs =                  (* ' *)
+  let f = Rt.pop_get ctxs in
+  let x = Rt.pop_eval ctxs in
+  match f, x with
+  | F { is_oper = _; instrs = _} as f, L xs ->
+     with_list ctxs @ 
+       fun () ->
+       let rec go idx =
+         if idx == -1 then ()
+         else (
+           Rt.push @ Array.get xs idx;
+           Rt.call_fn f ctxs;
+           go (idx - 1)
+         ) in
+       go @ Array.length xs - 1
+  | _ -> type_err "'"
+  
 let builtin =
   [("+",        true,   add);
    ("-",        true,   sub);
@@ -130,4 +149,5 @@ let builtin =
    (".",        true,   apply);
    ("]",        false,  list_end);
    ("[",        false,  list_begin);
-   ("/",        true,   fold)]
+   ("/",        true,   fold);
+   ("'",        true,   map)]
